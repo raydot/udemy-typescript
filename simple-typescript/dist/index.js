149194d1f11f3620fb21d8f792c6b95cb4e47d2c
Dave Kanter <moatgoat@hotmail.com>
2020-11-20 14:25:47 -0800

    Finished with modules section.

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -1,7 +1,31 @@
 "use strict";
-function multiply(a, b) {
-    return a * b;
-}
+// function multiply (a: number, b: number): number {
+//     return a*b
+// }
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import multiple, { multiplyByTwo } from './multiply'
+const multiplyModule = __importStar(require("./multiply"));
 const a = 4;
 const b = 7;
-console.log(`${a} * ${b} =  ${multiply(a, b)}`);
+// console.log(`${a} * ${b} =  ${multiply(a, b)}`)
+console.log(`${a} * ${b} =  ${multiplyModule.default(a, b)}`);
+multipleModule.multiplyByTwo(6);
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,9 +1,18 @@
-function multiply (a: number, b: number): number {
-    return a*b
-}
+// function multiply (a: number, b: number): number {
+//     return a*b
+// }
+
+//Various ways to import modules.
+// import multiply, { multiplyByTwo } from './multiply'
+// import * as multiplyModule from './multiply'
+import multiply, { multiplyByTwo as mBy2, HelloWorld } from './multiply'
 
 const a=4 
 const b=7
 
-console.log(`${a} * ${b} =  ${multiply(a, b)}`)
+// console.log(`${a} * ${b} =  ${multiply(a, b)}`)
+// console.log(`${a} * ${b} =  ${multiplyModule.default(a, b)}`)
+//multiplyModule.multiplyByTwo(6)
 
+console.log(`${a} * ${b} =  ${multiply(a, b)}`)
+mBy2(6);
